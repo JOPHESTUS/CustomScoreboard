f1993aaedfbcb1ad31797f6a430170d018cbae2c
JOPHESTUS <JOPHESTUS@jophest.us>
2014-04-27 21:59:24 +1000

    added metrics

--- src/main/java/us/jophest/customscoreboard/CustomScoreboard.java
+++ src/main/java/us/jophest/customscoreboard/CustomScoreboard.java
@@ -1,7 +1,6 @@
 package us.jophest.customscoreboard;
 
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.permission.Permission;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
@@ -14,7 +13,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -52,6 +54,12 @@ public class CustomScoreboard extends JavaPlugin implements Listener {
         SetupConfig();
 
         startTimer();
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            // Failed to submit the stats :-(
+        }
 
 
     }
